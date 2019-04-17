c2d3ccfbd384768c29eb51a36169cc729fea6f7d
Siddharth <siddharth.shah@elanic.in>
2019-04-17 16:28:24 +0530

    added a model and defined some basic methods in view and presenter interfaces

--- app/src/main/java/co/topnews/news_list/NewsListView.java
+++ app/src/main/java/co/topnews/news_list/NewsListView.java
@@ -1,4 +1,18 @@
 package co.topnews.news_list;
 
+import java.util.List;
+
+import co.topnews.news_list.models.NewsItem;
+
 public interface NewsListView {
+
+    void setData(List<NewsItem> newsItems);
+
+    void showError(String message);
+
+    void showProgress();
+
+    void hideProgress();
+
+
 }
---
+++ app/src/main/java/co/topnews/news_list/presenter/NewsListPresenter.java
@@ -0,0 +1,11 @@
+package co.topnews.news_list.presenter;
+
+import java.util.Map;
+
+public interface NewsListPresenter {
+
+    void getTopHeadlines(Map<String, String> queryParams);
+
+
+
+}
