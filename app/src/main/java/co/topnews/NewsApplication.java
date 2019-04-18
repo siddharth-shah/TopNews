98f8f73409098231cbd69f26bb8dcc28b713bd0a
Siddharth <siddharth.shah@elanic.in>
2019-04-18 18:41:48 +0530

    added application class

---
+++ app/src/main/java/co/topnews/NewsApplication.java
@@ -0,0 +1,12 @@
+package co.topnews;
+
+import android.app.Application;
+
+
+public class NewsApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
