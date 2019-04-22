fa26cd4b629691d90f52ef77cba2cf552b3a37a9
Siddharth <siddharth.shah@elanic.in>
2019-04-22 18:08:43 +0530

    creating application component in application class

--- app/src/main/java/co/topnews/NewsApplication.java
+++ app/src/main/java/co/topnews/NewsApplication.java
@@ -2,11 +2,32 @@ package co.topnews;
 
 import android.app.Application;
 
+import co.topnews.core.DaggerNewsApplicationComponent;
+import co.topnews.core.NewsApplicationComponent;
+import co.topnews.core.NewsApplicationModule;
+import co.topnews.core.network.ApiModule;
+import co.topnews.core.network.ClientModule;
+
 
 public class NewsApplication extends Application {
+
+    private static NewsApplicationComponent applicationComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        applicationComponent = createComponent();
+    }
+
+    private NewsApplicationComponent createComponent() {
+        return DaggerNewsApplicationComponent.builder()
+                .apiModule(new ApiModule())
+                .clientModule(new ClientModule())
+                .newsApplicationModule(new NewsApplicationModule())
+                .build();
+    }
+
+    public static NewsApplicationComponent getApplicationComponent() {
+        return applicationComponent;
     }
 }
