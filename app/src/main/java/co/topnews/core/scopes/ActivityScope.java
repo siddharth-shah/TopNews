d7d554be84284b9f729f540c3ec784229198952a
Siddharth <siddharth.shah@elanic.in>
2019-04-22 17:42:51 +0530

    Added scope

---
+++ app/src/main/java/co/topnews/core/scopes/ActivityScope.java
@@ -0,0 +1,15 @@
+package co.topnews.core.scopes;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by Jay Rambhia on 28/12/15.
+ */
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityScope {
+}
