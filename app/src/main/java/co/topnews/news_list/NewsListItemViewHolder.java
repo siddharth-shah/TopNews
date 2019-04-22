6b6204f342600c8b35926742f41c88910bcc47d3
Siddharth <siddharth.shah@elanic.in>
2019-04-22 18:29:47 +0530

    added adapter and viewholder for recyclerview

---
+++ app/src/main/java/co/topnews/news_list/NewsListItemViewHolder.java
@@ -0,0 +1,11 @@
+package co.topnews.news_list;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class NewsListItemViewHolder extends RecyclerView.ViewHolder {
+    public NewsListItemViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
