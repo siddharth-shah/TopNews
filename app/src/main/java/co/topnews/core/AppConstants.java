43b6bf13507ab47cb5783b1f8ece7004551555f7
Siddharth <siddharth.shah@elanic.in>
2019-04-22 18:06:49 +0530

    added base url and apikey

--- app/src/main/java/co/topnews/core/AppConstants.java
+++ app/src/main/java/co/topnews/core/AppConstants.java
@@ -1,5 +1,7 @@
 package co.topnews.core;
 
 public class AppConstants {
-    public static final String BASE_URL = "http://";
+    public static final String BASE_URL = "https://newsapi.org/";
+    public static final String API_KEY = "158b3705f4ec43ca913cbb7102c83359";
+
 }
