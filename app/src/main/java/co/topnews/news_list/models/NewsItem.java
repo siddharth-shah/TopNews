1f757f65eddd416563975cec87691950adb0ab55
Siddharth <siddharth.shah@elanic.in>
2019-04-22 18:05:25 +0530

    fixed model

--- app/src/main/java/co/topnews/news_list/models/NewsItem.java
+++ app/src/main/java/co/topnews/news_list/models/NewsItem.java
@@ -1,27 +1,12 @@
 package co.topnews.news_list.models;
 
 public class NewsItem {
-    /*"source": {
-"id": null,
-"name": "Animenewsnetwork.com"
-},
-"author": null,
-"title": "Microsoft to Launch Xbox One S 'All-Digital Edition' in May - Anime News Network",
-"description": "Version without optical disc drive retails for US$249.99",
-"url": "https://www.animenewsnetwork.com/news/2019-04-17/microsoft-to-launch-xbox-one-s-all-digital-edition-in-may/.145812",
-"urlToImage": "https://www.animenewsnetwork.com/thumbnails/crop600x315/cms/news.2/145812/xboxonealldigital-hero.png",
-"publishedAt": "2019-04-17T05:02:00Z",
-"content": "Microsoft announced a new \"All-Digital Edition\" for its Xbox One S console that will launch on May 7 for US$249.99 (US$50 cheaper than the regular Xbox One S). The game will have no optical disc drive, but will have a one-terabyte hard drive, and will ship wi… [+523 chars]"
-}*/
-
-    String source;
     String id;
     String author;
     String title;
     String url;
     String urlToImage;
     String publishedAt;
-
 
 
 }
