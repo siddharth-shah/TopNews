e79751b0d1c8efa23860548d91eab7e91a875360
Siddharth <siddharth.shah@elanic.in>
2019-04-22 18:54:11 +0530

    initial setup for adapter for testing views

--- app/src/main/java/co/topnews/news_list/NewsListAdapter.java
+++ app/src/main/java/co/topnews/news_list/NewsListAdapter.java
@@ -1,25 +1,73 @@
 package co.topnews.news_list;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import co.topnews.R;
+import co.topnews.news_list.models.NewsItem;
 
 public class NewsListAdapter extends RecyclerView.Adapter {
 
+    private final Context context;
+    private static final int NEWS_ITEM = 1;
+    private static final int LOADING_MORE = 2;
+    List<NewsItem> newsItemList;
+
+    public NewsListAdapter(Context context) {
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        switch (getItemViewType(i)) {
+            case 1:
+                return new NewsListItemViewHolder(LayoutInflater.from(context).inflate(R.layout.new_list_item, viewGroup));
+            default:
+                throw new IllegalArgumentException("View type not supported");
+
+        }
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        if (viewHolder instanceof NewsListItemViewHolder) {
+            final NewsItem newsItem = newsItemList.get(i);
+            ((NewsListItemViewHolder) viewHolder).getNewsTitle().setText(newsItem.getTitle());
+            ((NewsListItemViewHolder) viewHolder).getNewsDescription().setText(newsItem.getDescription());
+        }
+
+    }
+
+    public void setData(List<NewsItem> newsItems) {
+        if (newsItemList == null) {
+            newsItemList = new ArrayList<>();
+
+        } else {
+            newsItemList.clear();
+        }
+
+        newsItemList.addAll(newsItems);
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (newsItemList == null)
+            return 0;
+        return newsItemList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return NEWS_ITEM;
     }
 }
--- app/src/main/java/co/topnews/news_list/NewsListItemViewHolder.java
+++ app/src/main/java/co/topnews/news_list/NewsListItemViewHolder.java
@@ -3,9 +3,39 @@ package co.topnews.news_list;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import co.topnews.R;
 
 public class NewsListItemViewHolder extends RecyclerView.ViewHolder {
+    private ImageView newsImage;
+    private TextView newsTitle;
+    private TextView newsDescription;
+    private TextView publishedAt;
+
     public NewsListItemViewHolder(@NonNull View itemView) {
         super(itemView);
+        newsImage = itemView.findViewById(R.id.news_image);
+        newsTitle = itemView.findViewById(R.id.title);
+        newsDescription = itemView.findViewById(R.id.description);
+        publishedAt = itemView.findViewById(R.id.published_at);
+    }
+
+
+    public ImageView getNewsImage() {
+        return newsImage;
+    }
+
+    public TextView getNewsTitle() {
+        return newsTitle;
+    }
+
+    public TextView getNewsDescription() {
+        return newsDescription;
+    }
+
+    public TextView getPublishedAt() {
+        return publishedAt;
     }
 }
