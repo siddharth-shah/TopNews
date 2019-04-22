81b5ef8c6a81e1dddd9f46b739c2d622c8c5d52d
Siddharth <siddharth.shah@elanic.in>
2019-04-22 19:44:34 +0530

    added query param constants

--- app/src/main/java/co/topnews/news_list/data/api/NewsListApi.java
+++ app/src/main/java/co/topnews/news_list/data/api/NewsListApi.java
@@ -8,6 +8,11 @@ import retrofit2.http.GET;
 import retrofit2.http.QueryMap;
 
 public interface NewsListApi {
+    String QUERY_PARAM_COUNTRY_CODE = "country";
+    String QUERY_PARAM_API_KEY = "apiKey";
+    String QUERY_PARAM_PAGE = "page";
+    String QUERY_PARAM_PAGE_SIZE = "pageSize";
+
 
     @GET("/v2/top-headlines")
     Observable<TopHeadlinesResponseItem> getTopHeadLines(@QueryMap Map<String, String> queryParams);
